5820d2fc346d2f232f2400434ae1745f28efe292
Matias <matiasezequielmeira@gmail.com>
2025-12-26 00:43:19 -0300

    estructura Sistema

---
+++ src/main/java/com/matiasmeira/generador_plantillas/GeneradorPlantillasApplication.java
@@ -0,0 +1,13 @@
+package com.matiasmeira.generador_plantillas;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GeneradorPlantillasApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GeneradorPlantillasApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/matiasmeira/generador_plantillas/GeneradorPlantillasApplicationTests.java
@@ -0,0 +1,13 @@
+package com.matiasmeira.generador_plantillas;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GeneradorPlantillasApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
