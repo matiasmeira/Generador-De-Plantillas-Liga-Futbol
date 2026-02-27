0d4f0b04675ca4db5085c258cc4272785d0084de
matias <matiasezequielmeira@gmail.com>
2026-02-26 23:34:53 -0300

    import java list

--- src/main/java/com/matiasmeira/generador_plantillas/repository/EquipoRepository.java
+++ src/main/java/com/matiasmeira/generador_plantillas/repository/EquipoRepository.java
@@ -1,8 +1,12 @@
 package com.matiasmeira.generador_plantillas.repository;
 
-import com.matiasmeira.generador_plantillas.model.Equipo;
+import java.util.List;
+
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
+
+import com.matiasmeira.generador_plantillas.model.Equipo;
 
 @Repository
 public interface EquipoRepository extends JpaRepository<Equipo, Long> {
